bacb6e345012648224ed536264df6093499ffbfe
Kunal Shinde <124497605+KUNALSHINDE1515@users.noreply.github.com>
2023-12-11 15:18:30 +0000

    done chapter

---
+++ 01_variable.js
@@ -0,0 +1,19 @@
+const accountId = 142547;
+let accountEmail = "kunakshinde@7956.com";
+var accountPassword ='12345';
+accountCity = 'jaipur';
+
+let accountState;
+
+// accountId = 2;   not allowed
+// console.log(accountId);
+
+accountEmail ="kshs@";
+accountPassword=78956;
+accountCity = "Pune"
+console.log(accountId);
+
+console.table([accountEmail,accountPassword,accountCity,accountState]);
+
+
+// prefer not use var  because of issue in block scope and functional scope
---
+++ 02_datatypes.js
@@ -0,0 +1,24 @@
+"use strict"; //treat all JS code as new version
+
+// alert(3+3); // we are usinh nodejs not browzer
+
+console.log(3+3)
+console.log("kunal"); //code redablity that shoud be high
+
+let name ="kunal"
+let age =20
+let isLoogedIn = false
+
+// number --> 2 to power 53
+// bigint
+// string -->""
+// boolean --> true /false
+// null --> Stand alone value
+// undefined -->
+// symbol --> unique
+
+
+//object
+
+console.log(typeof null);  //undefined
+console.log(typeof undefined); // object
