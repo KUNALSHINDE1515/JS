d7d52e2d9a2b5e0ca6a6e14f2a0d5591565021fe
Kunal Shinde <124497605+KUNALSHINDE1515@users.noreply.github.com>
2023-12-11 15:39:15 +0000

    conversion of datatypes

---
+++ 03_conversionOperation.js
@@ -0,0 +1,25 @@
+let score = "33abc";
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+// let valueInNumber = Number(score)
+// console.log(typeof score)
+// console.log(valueInNumber);
+
+// "33" --> 33
+//"33abc" --> Nan(Not a number)
+//true --> 1; false -->0
+
+let isLoggedIn = "KUnal"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+// 1 --> true ; 0 --> false
+//"" --> false
+//"kunal" --> true
+
+let someNumber = 45;
+let stringNumber = String(someNumber);
+console.log(typeof stringNumber);
