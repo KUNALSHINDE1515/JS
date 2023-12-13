d816c276c864b80fde853ffc41922096986b26d8
Kunal Shinde <124497605+KUNALSHINDE1515@users.noreply.github.com>
2023-12-13 21:38:11 +0530

    Add files via upload

---
+++ JavaSript Pratice/1.js
@@ -0,0 +1,9 @@
+// write a javasrip prograam to store name phone number and marks of a student usinf objject
+
+const student ={
+    name: "kunal shinde",
+    phoneNumber: 965874521,
+    marks:121
+}
+
+console.log(student);
---
+++ JavaSript Pratice/2.js
@@ -0,0 +1,7 @@
+// Create a variable of type string and try to add a number to it.
+
+let v1 ="Kunal"
+let V2 = 2255
+
+console.log( v1  + V2);
+
