f8da8034603e5e207ea79cd6ca3e931a4b83918e
Kunal Shinde <124497605+KUNALSHINDE1515@users.noreply.github.com>
2023-12-16 09:05:16 +0000

    date and time

---
+++ date_and_time.js
@@ -0,0 +1,31 @@
+//
+
+let myDate = new Date()
+console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toISOString());
+// console.log(myDate.toJSON());
+// console.log(myDate.toLocaleDateString());
+// console.log(myDate.toLocaleString());
+// console.log(myDate.toLocaleTimeString());
+// console.log(myDate.toTimeString());
+// console.log(myDate.toUTCString());
+// console.log(myDate.getTimezoneOffset());
+
+// console.log(typeof myDate);
+
+let myCreatedDate = new Date("01-14-2023")
+// console.log(myCreatedDate.toDateString());
+
+let myTimeStamp = Date.now()
+
+// console.log(myTimeStamp);
+// console.log(Math.floor(Date.now()/1000));
+
+
+let newDate = new Date()
+// console.log(newDate);
+// console.log(newDate.getMonth());
+// console.log(newDate.getDay());
+
+// `${newDate.getDay()} and the time`
