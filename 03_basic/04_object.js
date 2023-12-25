0fb0f9b5b6ec0d7ab2098a342cdc79245b53492e
Kunal Shinde <124497605+KUNALSHINDE1515@users.noreply.github.com>
2023-12-25 11:00:26 +0000

    oject v-4 Done

---
+++ 03_basic/04_object.js
@@ -0,0 +1,53 @@
+//const tinderUser = new Object() // this is single ton object
+//console.log(tinderUser);
+
+const tinderUser = {}
+
+tinderUser.id = "123abc"
+tinderUser.name = "kunal"
+tinderUser.isLoggedIn = false
+
+// console.log(tinderUser);
+
+const regularUser = {
+    email: "some@gmail.com",
+    fullname: {
+        userfullname: {
+            firstname:"kunal",
+            lastName:"shinde"
+        }
+    }
+}
+
+// console.log(regularUser.fullname.userfullname.lastName);
+
+
+const obj1 = {1:"a", 2:"b"}
+const obj2 = {3:"c", 4:"d"}
+
+// const obj3 = {obj1 , obj2}
+// const obj3 = Object.assign({},obj1, obj2)
+
+const obj3 = {...obj1, ...obj2}
+// console.log(obj3);
+
+const users = [
+    {
+        id:1,
+        email:"ks@gamil.com"
+    },
+    {
+        id:1,
+        email:"ks@gamil.com"
+    },
+    {
+        id:1,
+        email:"ks@gamil.com"
+    },
+]
+
+users[1].email
+console.log(tinderUser);
+
+console.log(Object.keys(tinderUser));
+console.log(Object.values(tinderUser));
