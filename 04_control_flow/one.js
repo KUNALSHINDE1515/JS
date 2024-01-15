2c0a74e2d40c83f91f0c1d9df837cf32f8fd8d1a
Kunal Shinde <124497605+KUNALSHINDE1515@users.noreply.github.com>
2024-01-15 07:20:14 +0000

    done

---
+++ 04_control_flow/one.js
@@ -0,0 +1,61 @@
+// if
+
+
+// const isUserloggedIn = true
+
+
+// const temperature = 40
+// if(temperature === 50){
+//     console.log("less than 50");
+//  }else {
+//     console.log("temperature is greater than 50");
+//  }
+
+// 2<=2
+// 3 !=2 
+// <,>, <=, >=, ==, !=, ===
+
+
+// if (2 == "2") {
+//     console.log("executed");
+// }
+
+
+// const score = 200
+
+// if(score > 100){
+//     const power = "fly"
+//     console.log(`user power: ${power}`);
+// }
+// console.log(`user power: ${power}`);
+
+
+const balance = 1000
+// if (balance > 500) console.log("test"),console.log("test2"); // dont use this type of code in industry
+
+// if (balance < 500) {
+//     console.log("less than");
+// }else if(balance < 750){
+//     console.log("less than 750");
+// }else if(balance < 900){
+//     console.log("less than 900");
+// }
+// else {
+//     console.log("less than 1200");
+// }
+
+
+const userLoggedIn = true
+const debitCard = true
+const looegInFromGoogle = false
+const loggedInFromEmail = true
+
+if (userLoggedIn && debitCard && 2==2) {
+    console.log(`allow to buy couses`);
+}
+
+if (looegInFromGoogle || loggedInFromEmail) {
+    console.log("user logged in");
+    
+}
+
