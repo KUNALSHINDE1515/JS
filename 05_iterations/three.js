1276ed7384891f11a8b34b9bcf738c567eccdf6b
Kunal Shinde <124497605+KUNALSHINDE1515@users.noreply.github.com>
2024-01-16 14:32:13 +0000

    watch one more time

---
+++ 05_iterations/three.js
@@ -0,0 +1,22 @@
+// for of
+
+// ["","",""]
+// [{},{},{}]
+
+
+const arr = [1,2,3,4,5,6]
+
+for (const num of arr) {
+    // console.log(num);
+}
+
+const greetings = "hello kunal"
+for (const greet of greetings) {
+    console.log(`each char is ${greet}`);
+    
+}
+
+// Maps
+
+const map = new Map()
+map.
